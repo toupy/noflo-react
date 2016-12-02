dc44181602cc0e0518a160b7025a1d924a9564f6
Anthony Kebert <anthony.kebert@gfi.fr>
2016-12-02 17:24:25 +0100

    MAJ de la liste des scripts à compiler

--- entry.js
+++ entry.js
@@ -1,9 +1,8 @@
-// document.write(require("./src/the-graph-tools.jsx"));
-document.write(require("./src/Node.jsx"));
-document.write(require("./src/Editor.jsx"));
-document.write(require("./src/tools/Tools.js"));
-document.write(require("./src/graph/node/NodeConfig.js"));
-// document.write(require("./src/tools/factories/svg/SVGFactory.js"));
-
+document.write(require("./src/graph/node/base/NodeBase.jsx"));
+document.write(require("./src/graph/node/NodeDragLayer.jsx"));
+document.write(require("./src/graph/Node.jsx"));
+document.write(require("./src/Editor/Editor.jsx"));
+document.write(require("./src/tools/Tools.jsx"));
+document.write(require("./src/graph/node/NodeConfig.jsx"));
 document.write(require("./src/app.jsx"));
 
