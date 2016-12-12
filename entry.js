537c5784d9477469774e55f28b2d61b7ca01539d
Anthony Kebert <anthony.kebert@gfi.fr>
2016-12-12 17:37:53 +0100

    update of entry list

--- entry.js
+++ entry.js
@@ -9,10 +9,9 @@ require("./src/graph/node/NodeConfig.jsx");
 require("./src/Redux/actions/actions.jsx");
 require("./src/Redux/actions/nodeActions.jsx");
 require("./src/Redux/reducers/reducers.jsx");
-require("./src/Redux/reducers/nodeReducers.jsx");
+require("./src/Redux/reducers/Node/reducers.jsx");
 require("./src/Redux/Components/Todo.jsx");
 require("./src/Redux/Components/TodoList.jsx");
-require("./src/Redux/Components/Link.jsx");
 require("./src/Redux/Components/App.jsx");
 
 require("./src/Redux/Containers/VisibleTodoList.jsx");
