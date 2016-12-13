902ad8da5f0fc8e7a07f41bac7a65f574e385755
Anthony Kebert <anthony.kebert@gfi.fr>
2016-12-13 18:13:05 +0100

    pas besoin des chemins de toutes les classe !

--- entry.js
+++ entry.js
@@ -1,20 +1,2 @@
-require("./src/graph/node/base/NodeBase.jsx");
-require("./src/graph/node/NodeDragLayer.jsx");
-require("./src/graph/Node.jsx");
-require("./src/Editor/Editor.jsx");
-require("./src/Editor/EditorConfig.jsx");
-require("./src/tools/Tools.jsx");
-require("./src/graph/node/NodeConfig.jsx");
-
-require("./src/Redux/actions/actions.jsx");
-require("./src/Redux/actions/nodeActions.jsx");
-require("./src/Redux/reducers/reducers.jsx");
-require("./src/Redux/reducers/Node/reducers.jsx");
-require("./src/Redux/Components/Todo.jsx");
-require("./src/Redux/Components/TodoList.jsx");
-require("./src/Redux/Components/App.jsx");
-
-require("./src/Redux/Containers/VisibleTodoList.jsx");
-
 require("./src/app.jsx");
 
