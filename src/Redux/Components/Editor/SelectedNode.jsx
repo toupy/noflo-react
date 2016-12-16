1ad67ea2513728092229e63f583867c83a71658e
Anthony Kebert <anthony.kebert@gfi.fr>
2016-12-16 18:06:35 +0100

    anti crash

--- src/Redux/Components/Editor/SelectedNode.jsx
+++ src/Redux/Components/Editor/SelectedNode.jsx
@@ -1,16 +1,20 @@
+"use strict";
 import React, { Component, PropTypes } from 'react';
-import _ from 'lodash'
 
 export default class SelectedNode extends Component {
 
     render () {
-        const { nodes, selectedNodeId } = this.props;
-        var node = _.find(nodes, {id: selectedNodeId});
+        const { selectedNode } = this.props;
+
+        if(selectedNode === undefined){
+            return (null)
+        }
+
         return (
             <div>
-                <h2 className="label">{node.label}</h2>
-                <h3 className="label">{node.sublabel}</h3>
-                <strong>X : </strong>{node.x} <strong>Y : </strong>{node.y}
+                <h2 className="label">{selectedNode.label}</h2>
+                <h3 className="label">{selectedNode.sublabel}</h3>
+                <strong>X : </strong>{selectedNode.x} <strong>Y : </strong>{selectedNode.y}
             </div>
         )
     }
